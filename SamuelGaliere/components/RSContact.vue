0c489ae0e69f493d744664232d3ef14eed685298
Samuel <samuelgaliere@gmail.com>
2020-11-24 18:14:20 +0100

    Second Commit: Finalisation de la mise en place du skelete et travail sur le CSS du header.

---
+++ SamuelGaliere/components/RSContact.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img :src="icon"><a :href="accountLink" target="_blank">{{ accountName }}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RSContact",
+    props: ["icon", "accountLink", "accountName"]
+}
+</script>
+
+<style>
+
+</style>
--- SamuelGaliere/pages/index.vue
+++ SamuelGaliere/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <div id="header">
+    <div class="header">
+      <h2 class="subtitle">Hi, I'm</h2>
       <h1 class="title">Samuel GALIERE</h1>
-      <img id="background" src="../images/background.jpg" />
+      <h2 id="job" class="subtitle"> and I'm a developper.</h2>
+      <button onClick="javascript:window.location.href='https://pornhub.com'">Edit</button>
+      <img id="background" src="../images/background.jpg"/>
     </div>
     <div>
       <DescriptionBlock 
@@ -54,6 +57,23 @@
     <div>
       <FormulaireContact/>
     </div>
+    <div>
+      <RSContact
+      icon="www.faceuibefoê.com"
+      accountLink="https://www.google.fr/"
+      accountName="Samuel GALIERE"/>
+      <RSContact
+      icon="www.faceuibefoê.com"
+      accountLink="https://www.google.fr/"
+      accountName="Samuel GALIERE"/>
+      <RSContact
+      icon="www.faceuibefoê.com"
+      accountLink="https://www.google.fr/"
+      accountName="Samuel GALIERE"/>
+
+
+
+    </div>
   </div>
 </template>
 
@@ -62,10 +82,11 @@ import DescriptionBlock from '../components/DescriptionBlock.vue'
 import ExperienceDetail from '../components/ExperienceDetail.vue'
 import ExperienceVignette from '../components/ExperienceVignette.vue'
 import FormulaireContact from '../components/FormulaireContact.vue'
+import RSContact from '../components/RSContact.vue'
 import Skill from '../components/Skill.vue'
 
 export default {
-  components: { DescriptionBlock, ExperienceDetail, ExperienceVignette, Skill, FormulaireContact },
+  components: { DescriptionBlock, ExperienceDetail, ExperienceVignette, Skill, FormulaireContact, RSContact },
 
 }
 </script>
@@ -91,33 +112,50 @@ export default {
     Arial,
     sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #accaeb;
+  font-weight: 350;
+  font-size: 5vw;
+  color: #88bedd;
   letter-spacing: 1px;
-  position: absolute; 
+  position: absolute;
+  left: 7%;
+  top: 25.5%; 
+  
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  position: absolute;
+  left: 7%;
+  top: 20%; 
+  font-size: 3vw;
+  color: #a1a7adcc;
+  font-weight: 100;
+  letter-spacing: 1px;
 }
 
-.links {
-  padding-top: 15px;
+#job {
+  left: 15%;
+  top: 39%; 
 }
 
-#header {
-  position: relative;  
-  text-align: center;
+#background { 
+  margin-left: 50%;
+  height: auto; 
+  margin-top: 0.2%;
 }
 
-#background {
-  width: 100%;
-
+.header {
+  background-color: rgba(182, 182, 182, 0.342);
 }
 
 </style>
